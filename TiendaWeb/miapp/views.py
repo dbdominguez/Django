5bdfb89290b43f9d3c2c8dbcc1fc34cc80e08490
cote-fredes <ma.fredesh@duocuc.cl>
2025-04-18 00:05:23 -0400

    Update views.py

--- TiendaWeb/miapp/views.py
+++ TiendaWeb/miapp/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegistroUsuarioForm
 
-# Create your views here.
+def registro_usuario(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, 'registro.html', {'form': form})
 
+def login_usuario(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            return render(request, 'login.html', {'error': 'Credenciales inválidas'})
+    return render(request, 'login.html')
+
+def logout_usuario(request):
+    logout(request)
+    return redirect('login')
 # General.
 def inicio(request):
     return render(request, "Index.html")
