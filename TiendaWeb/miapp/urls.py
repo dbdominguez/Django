fd39a38bbe2b25f42e62ad53bb45a4f81827d8db
cote-fredes <ma.fredesh@duocuc.cl>
2025-04-18 00:04:36 -0400

    Update urls.py

--- TiendaWeb/miapp/urls.py
+++ TiendaWeb/miapp/urls.py
@@ -1,1 +1,15 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('registro/', views.registro_usuario, name='registro'),
+    path('login/', views.login_usuario, name='login'),
+    path('logout/', views.logout_usuario, name='logout'),
+]
+urlpatterns += [
+    path('', views.inicio, name='inicio'),
+    path('carrito/', views.ver_carrito, name='ver_carrito'),
+    path('agregar/<int:producto_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('comprar/', views.procesar_compra, name='procesar_compra'),
+    path('historial/', views.historial_compras, name='historial_compras'),
+]
