aeddbb526b1b7026bdae8409cd1e8abc4b2bd2a1
cote-fredes <ma.fredesh@duocuc.cl>
2025-04-18 00:02:24 -0400

    Update admin.py

--- TiendaWeb/miapp/admin.py
+++ TiendaWeb/miapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario, Producto, Categoria, Carrito, ItemCarrito, Compra, DetalleCompra
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Carrito)
+admin.site.register(ItemCarrito)
+admin.site.register(Compra)
+admin.site.register(DetalleCompra)
