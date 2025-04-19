a3aaaa12fa72123282a8bd8b80772af7116e5e7b
cote-fredes <ma.fredesh@duocuc.cl>
2025-04-19 15:35:13 -0400

    Update settings.py

--- TiendaWeb/TiendaWeb/settings.py
+++ TiendaWeb/TiendaWeb/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'TiendaWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'XE',
+        'USER': 'c###proy',
+        'PASSWORD': 'proy',
+        'HOST': 'localhost',
+        'PORT':  '1521',
     }
-}
+} 
 
 
 # Password validation
