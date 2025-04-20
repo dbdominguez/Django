7976ab6ac89590d8c3bb68cecade9e71189a3aed
dbdominguez <d.dominguezg@duocuc.cl>
2025-04-20 01:31:28 -0400

    Prueba Conexion

--- TiendaWeb/TiendaWeb/settings.py
+++ TiendaWeb/TiendaWeb/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +78,11 @@ WSGI_APPLICATION = 'TiendaWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'XE',
-        'USER': 'c###proy',
-        'PASSWORD': 'proy',
-        'HOST': 'localhost',
-        'PORT':  '1521',
+        'NAME': 'localhost/XEPDB1',
+        'USER': 'django2',
+        'PASSWORD': 'Djangouser123',
     }
-} 
+}
 
 
 # Password validation
