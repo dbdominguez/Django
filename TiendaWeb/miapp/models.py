c59ab8d7dd972d72b81cd1ac1c14994181913526
dbdominguez <d.dominguezg@duocuc.cl>
2025-04-20 19:23:41 -0400

    Models actualizado, no seguro si es funcional pero para guiarse con los nombres para crear tablas en oracle

--- TiendaWeb/miapp/models.py
+++ TiendaWeb/miapp/models.py
@@ -1,70 +1,119 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    
-    def __str__(self):
-        return self.nombre
 
-class Juego(models.Model):
-    nombre = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    precio = models.IntegerField()
-    stock = models.IntegerField()
-    imagen = models.CharField(max_length=255)  # Path a la imagen
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    fecha_lanzamiento = models.DateField()
-    desarrollador = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.nombre
-
-class Perfil(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    telefono = models.CharField(max_length=15)
-    direccion = models.CharField(max_length=200)
-    region = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.usuario.username
-
-class Orden(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    completado = models.BooleanField(default=False)
-    tipo_documento = models.CharField(max_length=20, choices=[('boleta', 'Boleta'), ('factura', 'Factura')])
-    metodo_entrega = models.CharField(max_length=20, choices=[('envio', 'Envío a domicilio'), ('retiro', 'Retiro en local')])
-    
-    def __str__(self):
-        return f'Orden {self.id} - {self.usuario.username}'
-    
-    @property
-    def get_total(self):
-        items = self.ordenitem_set.all()
-        total = sum([item.get_total for item in items])
-        return total
-
-class OrdenItem(models.Model):
-    juego = models.ForeignKey(Juego, on_delete=models.SET_NULL, null=True)
-    orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
-    cantidad = models.IntegerField(default=1)
-    fecha_agregado = models.DateTimeField(auto_now_add=True)
-    
-    @property
-    def get_total(self):
-        return self.juego.precio * self.cantidad
-
-class Valoracion(models.Model):
-    juego = models.ForeignKey(Juego, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    puntuacion = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
-    comentario = models.TextField(blank=True)
-    fecha = models.DateTimeField(auto_now_add=True)
-    
+# ROL Tabla
+class Rol(models.Model):
     class Meta:
-        unique_together = ('juego', 'usuario')
-    
+        db_table = 'MAIN_ROL'
+    id_rol = models.AutoField(primary_key=True, db_column='ID_ROL')
+    nombre = models.CharField(max_length=50, db_column='NOMBRE')
+
     def __str__(self):
-        return f'{self.usuario.username} - {self.juego.nombre} - {self.puntuacion}'
+        return self.nombre
+
+
+#Inicio Sesion Autenticacion
+class UsuarioManager(BaseUserManager):
+    def create_user(self, correo, password=None, **extra_fields):
+        if not correo:
+            raise ValueError("El correo es obligatorio")
+        correo = self.normalize_email(correo)
+        user = self.model(correo=correo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, correo, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(correo, password, **extra_fields)
+
+
+# USUARIO Tabla
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        db_table = 'USUARIO'
+
+    id_usuario = models.AutoField(primary_key=True, db_column='ID_USUARIO')
+    nombre = models.CharField(max_length=100, db_column='NOMBRE', null=True)
+    apellido = models.CharField(max_length=100, db_column='APELLIDO', null=True)
+    rol = models.ForeignKey('MAIN_Rol', on_delete=models.PROTECT, db_column='ID_ROL')
+    correo = models.EmailField(unique=True, db_column='CORREO')
+    direccion = models.CharField(max_length=255, db_column='DIRECCION', null=True, blank=True)
+    telefono = models.CharField(max_length=20, db_column='TELEFONO', null=True, blank=True)
+    clave = models.CharField(max_length=255, db_column='CLAVE')
+    is_active = models.BooleanField(default=True, db_column='ACTIVO')
+    is_staff = models.BooleanField(default=False)
+
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = 'correo'
+    REQUIRED_FIELDS = ['nombre', 'apellido']
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido} ({self.correo})'
+
+
+# CATEGORIA Tabla
+class Categoria(models.Model):
+    class Meta:
+        db_table = 'CATEGORIA'
+    id_categoria = models.AutoField(primary_key=True, db_column='ID_CATEGORIA')
+    nombre = models.CharField(max_length=50, db_column='NOMBRE')
+
+
+# PLATAFORMA Tabla
+class Plataforma(models.Model):
+    class Meta:
+        db_table = 'PLATAFORMA'
+    id_categoria = models.AutoField(primary_key=True, db_column='ID_PLATAFORMA')
+    nombre = models.CharField(max_length=50, db_column='NOMBRE')
+
+
+# PRODUCTO tabla
+class Producto(models.Model):
+    class Meta:
+        db_table = 'PRODUCTO'
+    id_producto = models.AutoField(primary_key=True, db_column='ID_PRODUCTO')
+    nombre = models.CharField(max_length=100, db_column='NOMBRE')
+    descripcion = models.TextField(db_column='DESCRIPCION', null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, db_column='PRECIO')
+    stock = models.IntegerField(default=0, db_column='STOCK')
+    imagen_url = models.CharField(max_length=255, db_column='IMAGEN_URL', null=True, blank=True)
+    plataforma = models.ForeignKey(Plataforma, on_delete=models.CASCADE, db_column='ID_PLATAFORMA')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column='ID_CATEGORIA')
+
+
+#CARRITO TABLA
+class Carrito(models.Model):
+    class Meta:
+        db_table = 'CARRITO'
+    id_carrito = models.AutoField(primary_key=True, db_column='ID_CARRITO')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column='ID_USUARIO')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, db_column='ID_PRODUCTO')
+    cantidad = models.PositiveIntegerField(db_column='CANTIDAD')
+
+
+#COMPRA TABLA
+class Compra(models.Model):
+    class Meta:
+        db_table = 'COMPRA'
+    id_compra = models.AutoField(primary_key=True, db_column='ID_COMPRA')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column='ID_USUARIO')
+    fecha = models.DateTimeField(auto_now_add=True, db_column='FECHA')
+    total = models.DecimalField(max_digits=10, decimal_places=2, db_column='TOTAL')
+    estado = models.CharField(max_length=50, db_column='ESTADO')
+
+
+#DETALLECOMPRA Tabla
+class DetalleCompra(models.Model):
+    class Meta:
+        db_table = 'DETALLE_COMPRA'
+    id_detalle = models.AutoField(primary_key=True, db_column='ID_DETALLE')
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, db_column='ID_COMPRA')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, db_column='ID_PRODUCTO')
+    cantidad = models.PositiveIntegerField(db_column='CANTIDAD')
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2, db_column='PRECIO_UNITARIO')
+
